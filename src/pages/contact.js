6620a8baee7633c8660d8926cf3145c279f3f860
Gilles Rusca <gilles.rusca@gmail.com>
2021-03-29 09:06:35 -0600

    Revert "added netlify form"

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -45,44 +45,18 @@ const Contact = () => {
       <header>
         <h1>Contact</h1>
       </header>
-      {/* <form
+      <form
         name='contact-form'>
         <div className='fields'>
           <input type='text' name='first-name' placeholder='First Name' required/>
           <input type='text' name='last-name' placeholder='Last Name' required/>
         </div>
         <div className='fields'>
-          <input
-          type='tel'
-          name='phone'
-          pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' placeholder='Format: 123-456-7890' required/>
+          <input type='tel' id='phone' name='phone' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' placeholder='Format: 123-456-7890' required/>
           <input type='email' name='email' placeholder='Email' required/>
         </div>
-        <textarea
-         name='message' placeholder='Your Message' className='full-width' rows='13'required/>
+        <textarea name='message' placeholder='Your Message' className='full-width' rows='13'required/>
         <input type='button' value='SEND'/>
-      </form> */}
-      <form method='post' netlify-honeypot='bot-field' data-netlify='true' name='contact'>
-        <input type='hidden' name='bot-field' />
-        <input type='hidden' name='form-name' value='contact' />
-        <label>
-          Name
-          <input type='text' name='name' id='name' />
-        </label>
-        <label>
-          Email
-          <input type='email' name='email' id='email' />
-        </label>
-        <label>
-          Subject
-          <input type='text' name='subject' id='subject' />
-        </label>
-        <label>
-          Message
-          <textarea name='message' id='message' rows='5' />
-        </label>
-        <button type='submit'>Send</button>
-        <input type='reset' value='Clear' />
       </form>
 
     </StyledSection>
