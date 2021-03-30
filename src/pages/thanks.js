4fa85e789c5111d575dfa2740477289b523b656e
Gilles Rusca <gilles.rusca@gmail.com>
2021-03-30 12:07:01 -0600

    added thank page

---
+++ src/pages/thanks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Thanks = () => (
+  <>
+    <h1>Thank you!</h1>
+    <p>This is a custom thank you page for form submissions</p>
+  </>
+);
+
+export default Thanks;
