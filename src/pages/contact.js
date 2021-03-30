2116860be2f7d3da4a6f221fbc8303e1c4d835c0
Gilles Rusca <gilles.rusca@gmail.com>
2021-03-30 12:07:30 -0600

    Stateful React Form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { navigate } from 'gatsby';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const StyledSection = styled.section`
@@ -39,27 +40,55 @@ const StyledSection = styled.section`
     }
 `;
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+};
+
 const Contact = () => {
+  const [formState, setFormState] = useState({});
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...formState,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error));
+  };
   return (
     <StyledSection className='page-width'>
       <header>
         <h1>Contact</h1>
       </header>
       <form
+        name='contact-form'
         method='post'
+        action='/thanks/'
+        data-netlify='true'
         netlify-honeypot='bot-field'
-        data-netlify='true' name='contact-form'>
-        <input type='hidden' name='bot-field' />
-        <input type='hidden' name='form-name' value='contact' />
+        onSubmit={handleSubmit}
+      >
+        <input type='hidden' name='bot-field' onChange={handleChange}/>
+        <input type='hidden' name='form-name' value='contact' onChange={handleChange}/>
         <div className='fields'>
-          <input type='text' name='first-name' placeholder='First Name' required/>
-          <input type='text' name='last-name' placeholder='Last Name' required/>
+          <input type='text' name='first-name' placeholder='First Name' onChange={handleChange} required/>
+          <input type='text' name='last-name' placeholder='Last Name' onChange={handleChange} required/>
         </div>
         <div className='fields'>
-          <input type='tel' id='phone' name='phone' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' placeholder='Format: 123-456-7890' required/>
-          <input type='email' name='email' placeholder='Email' required/>
+          <input type='tel' id='phone' name='phone' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' placeholder='Format: 123-456-7890' onChange={handleChange} required/>
+          <input type='email' name='email' placeholder='Email' onChange={handleChange} required/>
         </div>
-        <textarea name='message' placeholder='Your Message' className='full-width' rows='13'required/>
+        <textarea name='message' placeholder='Your Message' className='full-width' rows='13' onChange={handleChange}required/>
         <input type='button' value='SEND'/>
       </form>
 
