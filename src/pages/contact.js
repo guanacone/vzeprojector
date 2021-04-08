208b076e7776886c7e0162aba29f5cbb77dcdc83
Gilles Rusca <gilles.rusca@gmail.com>
2021-04-08 16:17:13 -0600

    edited form handling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import SEO from '../components/SEO';
 
 const StyledSection = styled.section`
   form {
     font-size: 1.6rem;
-
+    
     input, textarea {
       box-sizing: border-box;
       padding: 10px;
@@ -47,9 +48,51 @@ const StyledSection = styled.section`
       }
     }
   }
+
+  div {
+    text-align: center;
+  }
+
+  .hidden {
+      display: none;
+    }
 `;
 
 const Contact = () => {
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null,
+  });
+  const [msgSent, setMsgSent] = useState(false);
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    });
+    console.log({ serverState });
+    if (ok) {
+      form.reset();
+    }
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    setServerState({ submitting: true });
+    axios({
+      method: 'post',
+      url: 'https://getform.io/f/7409e855-3dc6-4b91-bfe0-49074fa3994f',
+      data: new FormData(form),
+    })
+      .then(() => {
+        setMsgSent(true);
+        handleServerResponse(true, 'Success!', form);
+      })
+      .catch((r) => {
+        handleServerResponse(false, r.response.data.error, form);
+        alert('There was en error, please try again');
+      });
+  };
+  console.log({ serverState });
   return (
     <>
       <SEO title='Contact Us'/>
@@ -57,8 +100,11 @@ const Contact = () => {
         <header>
           <h1>Contact</h1>
         </header>
-        <form
-          name='contact-form'>
+        <div className={msgSent ? null : 'hidden'}>
+          <h2>Your message has been submitted. Thank you!</h2>
+          <h3>We will shortly get back to you</h3>
+        </div>
+        <form onSubmit={handleOnSubmit} className={msgSent ? 'hidden' : null}>
           <div className='fields'>
             <input type='text' name='first-name' placeholder='First Name' required/>
             <input type='text' name='last-name' placeholder='Last Name' required/>
@@ -68,9 +114,8 @@ const Contact = () => {
             <input type='email' name='email' placeholder='Email' required/>
           </div>
           <textarea name='message' placeholder='Your Message' className='full-width' rows='13'required/>
-          <input type='button' value='SEND'/>
+          <button type='submit' disabled={serverState.submitting}>{serverState.submitting ? 'Sending...' : 'Send'}</button>
         </form>
-
       </StyledSection>
     </>
   );
